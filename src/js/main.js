61a3e091726e61c46ca447f2e6a918691aabce28
KseniaMV <marks-83@mail.ru>
2020-11-11 22:19:48 +0300

    fix: change menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,87 @@
-let navList = document.querySelectorAll(".navigation-item");
-    navList.forEach(item =>{
-        item.addEventListener("click", (e)=>{
-            if(e.target.classList.contains("item--light-theme")){
-                let link = e.target.childNodes[0];
-                link.classList.add("showLink--light-theme");
+
+showlink();
+changeContent();
+
+function showlink(){
+    let navigationCircle = document.querySelectorAll(".navigation-circle");
+        navigationCircle.forEach(circle =>{
+            circle.addEventListener("click", (e)=>{
+                let targetItem = e.target.nextElementSibling;
+                    targetItem.classList.add("show-link");
+            })
+        }); 
+};
+
+function changeContent(){
+    let links = document.querySelectorAll(".navigation-link");
+        links.forEach(link => {
+            link.addEventListener("click", (e)=>{
+                let targetLink = e.target;
+                let pageId = targetLink.id;
+                let downLoadPage = new DownloadPage(pageId);
+                let content = downLoadPage.getPageHTML();
+                    content.then((content)=>replaceContent(content))
+                    .then(()=>changeTheme(pageId));
+                    hideLinks();
+            })
+            
+        });
+}
+
+function replaceContent(content){
+    let main = document.querySelector(".main");
+    main.innerHTML = content;
+
+}
+
+
+function hideLinks(){
+    let links = document.querySelectorAll(".navigation-link");
+        links.forEach(link =>{
+            if(link.classList.contains("show-link")){
+                link.classList.remove("show-link");
             }
-            if(e.target.classList.contains("item--dark-theme")){
-                let link = e.target.childNodes[0];
-                link.classList.add("showLink--dark-theme");
-            }
+
         })
+}
 
-    })
+function changeTheme(pageName){
+    let name = document.querySelector(".title-name");
+    let profession = document.querySelector(".title-profession");
+    let nav = document.querySelectorAll(".navigation-item");
+    switch (pageName) {
+        case "home":
+ 
+            break;
+        
+        case "about":
+            name.classList.add("title--dark-theme");
+            profession.classList.add("title--dark-theme");
+            nav.forEach(item=> {
+
+            });
+            break;
+        case "portfolio":
+                
+            break;
+        case "contacts":
+                
+            break;
+        default:
+            break;
+    }
+};
 
 
-let navigationLink = document.querySelectorAll(".navigation-link");
+    /*let navigationLink = document.querySelectorAll(".navigation-link");
     navigationLink.forEach(link => {
         link.addEventListener("click",(e)=>{
             let pageName = e.target.id;
             let downLoadPage = new DownloadPage(pageName);
             let content = downLoadPage.getPageHTML();
-            content.then((content) => changeContent(content))
-            .then(()=>changeTheme(pageName));
+                content.then((content) => changeContent(content))
+                .then(()=>changeTheme(pageName));
+            link.classList.add("hide-link");  
             if(link.classList.contains("showLink--light-theme")){
                 link.classList.remove("showLink--light-theme");
             }
@@ -29,39 +89,4 @@ let navigationLink = document.querySelectorAll(".navigation-link");
                 link.classList.remove("showLink--dark-theme");
             }
         })
-    });
-
-    function changeContent(content){
-        let main = document.querySelector(".main");
-        main.innerHTML = content;
-
-    }
-
-    function changeTheme(pageName){
-        let name = document.querySelector(".title-name");
-        let profession = document.querySelector(".title-profession");
-        let nav = document.querySelectorAll(".navigation-item");
-        switch (pageName) {
-            case "home":
- 
-                break;
-        
-            case "about":
-                name.classList.toggle("title--dark-theme");
-                profession.classList.toggle("title--dark-theme");
-                nav.forEach(item=> {
-                    item.classList.toggle("item--light-theme");
-                    item.classList.toggle("item--dark-theme");
-                });
-                break;
-            case "portfolio":
-                
-                break;
-            case "contacts":
-                
-                break;
-            default:
-                break;
-        }
-    }
-
+    });*/
